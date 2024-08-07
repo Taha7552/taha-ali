65e5870179ffefed1dd303bc60ff16b271491c80
Taha7552 <tahaalikhan1085904@gmail.com>
2024-08-07 23:09:56 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,88 @@
+/////alert///
+// alert ("hello world!")
+// alert ("ERROR!please enter a valid password")
+// alert ("welcome to JS land \n Happy Coding!")
+// alert = prompt ("enter your name")
+     ///////////end/////
+
+//////variables for strings////
+
+//  let message;
+//  message = 'hello world!';
+//  alert("message");  
+//   let myname;
+//   let age;
+//   let proffesion;                           
+//  myname = "taha khan"
+//  age = "18"
+//  proffesion = "student"
+//  alert(myname )
+//  alert(age )
+//  alert(proffesion)
+// alert("PIZZA\n PIZ\n PI\n P")
+// alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+// let txt1 = "my email address is"
+// let txt2 = "example@tahakhangmail.com"
+// alert( txt1 + txt2 )
+// 
+// let text1 = "YAH! i can write HTML";
+//  let text2 ="text using JS";
+// let result = text1 .concat(text2);
+// document.getElementById("container").innerHTML = result;
+            //////end///////
+
+
+///////variables for numbers////
+// let age; 
+// age = "I am 18years old"
+// alert(age)
+
+
+// let visit;
+// visit = 0
+// visit++;
+// alert("you visited this website" + visit + "times")
+
+// let birthyear;
+// birthyear = "I was born in 2005"
+// document.write(birthyear)
+
+
+// // Declare variables
+// let visitorName;
+// let productTitle;
+// let quantity;
+
+// // Assign values
+// visitorName = "taha";
+// productTitle = "T-shirt";
+// quantity = 5;
+
+// // Display message
+// document.write(`${visitorName} ordered ${quantity} ${productTitle}(s) on XYZ Clothing store`);
+
+
+////////////variable names : legal and illegal///////////
+let myname = "taha", age = 20, city = "karachi";
+    ////legal variable names/////
+// 1. firstName
+// 2. _myVariable
+// 3. totalAmount
+// 4. counter
+// 5. $result
+
+      ////////illegal variable names///////
+
+// 1. 1stVariable (starts with a number)
+// 2. my variable (contains a space)
+// 3. total-amount (contains a hyphen)
+// 4. let (is a reserved keyword)
+// 5. first.name (contains a dot)
+
+
+            //////////end////////
+
+            ////////chapter 5 maths///
+
+
+ 
